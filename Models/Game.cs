b244aff864640e94f8e7d116e9597a5cf49a3bb3
Alik Hansen <ahansen32@my.wctc.edu>
2026-09-13 15:47:55 -0500

    Game.cs added

---
+++ Models/Game.cs
@@ -0,0 +1,10 @@
+namespace FavoriteGamesGames.Web.Models;
+
+public class Game
+{
+    public int Id {get;set;}
+    public string Name {get;set;}="";
+    public string Developer {get;set;}="";
+    public int YearReleased {get;set;}
+
+}
