d226462e5d2710c8988aa866b27cf525915c4fb8
Alik Hansen <ahansen32@my.wctc.edu>
2026-09-13 15:18:56 -0500

    Initial project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FavoriteGames.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
